27fdbb5b6113aa33ccd982827acd5089266bc747
Carter <carreejoh4637@gmail.com>
2023-05-21 16:02:07 -0600

    Both api calls are working, just need to append info to document

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,39 @@ let searchedCity = document.querySelector('#citySearch');
 
 
 
+
+
+function findWeather(tempLat, tempLon) {
+    
+    fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + tempLat + '&lon=' + tempLon + '&appid=bd4cfacd3adbb7dbb84ff559c0b71f94')
+    .then((response) => {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+}
+
+
+
+
 citySearch.addEventListener('click', function() {
 
     let tempCity = searchedCity.value;
-    console.log(tempCity);
+    tempCity=tempCity.replace(/ /g,"_");
 
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=&appid=bd4cfacd3adbb7dbb84ff559c0b71f94')
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + tempCity + '&limit=&appid=bd4cfacd3adbb7dbb84ff559c0b71f94')
     .then((response) => {
         return response.json();
     })
     .then(function(data) {
         console.log(data[0]);
         if(data[0].country === "US") {
-
+            let tempLat = data[0].lat;
+            let tempLon = data[0].lon;
+            findWeather(tempLat, tempLon);
+        } else {
+            alert("Sorry this city is not in the USA");
         }
     })
 })
