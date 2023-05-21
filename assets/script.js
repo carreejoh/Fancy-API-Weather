233c6245460589085f25ef68aea6016a1aa54939
Carter <carreejoh4637@gmail.com>
2023-05-21 16:59:44 -0600

    Have begun to append info to screen

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,29 @@ let searchedCity = document.querySelector('#citySearch');
 
 
 
+function appendDataFuture(data) {
+    console.log(data);
+    $('#currentWeatherName').text(data.city.name);
+
+    $('#forecast1').children('h3').eq(0).text(data.list[4].dt_txt);
+    $('#forecast2').children('h3').eq(0).text(data.list[12].dt_txt);
+    $('#forecast3').children('h3').eq(0).text(data.list[20].dt_txt);
+    $('#forecast4').children('h3').eq(0).text(data.list[28].dt_txt);
+    $('#forecast5').children('h3').eq(0).text(data.list[36].dt_txt);
+
+    function tempConversion(temp) {
+        return Math.floor(9/5 * (temp-273) + 32);
+    }
+
+    $('#forecast1').children('h3').eq(1).text("temp: " + tempConversion(data.list[4].main.temp));
+    $('#forecast2').children('h3').eq(1).text("temp: " + tempConversion(data.list[12].main.temp));
+    $('#forecast3').children('h3').eq(1).text("temp: " + tempConversion(data.list[20].main.temp));
+    $('#forecast4').children('h3').eq(1).text("temp: " + tempConversion(data.list[28].main.temp));
+    $('#forecast5').children('h3').eq(1).text("temp: " + tempConversion(data.list[36].main.temp));
+}
+
+
+
 
 function findWeather(tempLat, tempLon) {
     
@@ -12,15 +35,12 @@ function findWeather(tempLat, tempLon) {
         return response.json();
     })
     .then(function(data) {
-        console.log(data);
+        appendDataFuture(data);
     })
-}
+};
 
 
-
-
-citySearch.addEventListener('click', function() {
-
+function findCoords() {
     let tempCity = searchedCity.value;
     tempCity=tempCity.replace(/ /g,"_");
 
@@ -38,4 +58,8 @@ citySearch.addEventListener('click', function() {
             alert("Sorry this city is not in the USA");
         }
     })
-})
+};
+
+
+
+citySearch.addEventListener('click', findCoords);
