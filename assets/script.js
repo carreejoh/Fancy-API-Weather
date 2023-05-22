c53320e236de63780b65182dd3de6b2aa5ef1338
Carter <carreejoh4637@gmail.com>
2023-05-22 14:47:04 -0600

    Added local storage

--- assets/script.js
+++ assets/script.js
@@ -35,7 +35,22 @@ function appendDataFuture(data) {
     $('#forecast3').children('h3').eq(3).text("Humidity: " + (data.list[20].main.humidity) + "%");
     $('#forecast4').children('h3').eq(3).text("Humidity: " + (data.list[28].main.humidity) + "%");
     $('#forecast5').children('h3').eq(3).text("Humidity: " + (data.list[36].main.humidity) + "%");
+};
+
+
+
+function appendCurrentWeather(data) {
+    console.log(data);
+
+    function tempConversion(temp) {
+        return Math.floor(9/5 * (temp-273) + 32);
+    }
+
+    $('#currentTemp').text("Temperature: " + tempConversion(data.main.temp));
+    $('#currentWind').text("Wind: " + data.wind.speed + "mph");
+    $('#currentHumdity').text("Humidity: " + data.main.humidity + "%");
 }
+
 
 
 
@@ -51,10 +66,37 @@ function findWeather(tempLat, tempLon) {
     })
 };
 
+function findWeatherCurrent(tempLat, tempLon) {
+    
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + tempLat + '&lon=' + tempLon + '&appid=bd4cfacd3adbb7dbb84ff559c0b71f94')
+    .then((response) => {
+        return response.json();
+    })
+    .then(function(data) {
+        appendCurrentWeather(data);
+    })
+}
+
+
+
+function loadLocalCities(tempCity) {
+    let loadCities = (localStorage.getItem('recentCities'));
+    if (loadCities === null) {
+        localStorage.setItem('recentCities', '["salt_lake_city"]');
+    } else {
+        let addACity = JSON.parse(localStorage.getItem('recentCities'));
+        addACity.push(tempCity);
+        localStorage.setItem('recentCities', JSON.stringify(addACity));
+    }
+}
+
+
 
 function findCoords() {
     let tempCity = searchedCity.value;
     tempCity=tempCity.replace(/ /g,"_");
+
+    loadLocalCities(tempCity);
 
     fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + tempCity + '&limit=&appid=bd4cfacd3adbb7dbb84ff559c0b71f94')
     .then((response) => {
@@ -66,6 +108,7 @@ function findCoords() {
             let tempLat = data[0].lat;
             let tempLon = data[0].lon;
             findWeather(tempLat, tempLon);
+            findWeatherCurrent(tempLat, tempLon);
         } else {
             alert("Sorry this city is not in the USA");
         }
@@ -75,3 +118,5 @@ function findCoords() {
 
 
 citySearch.addEventListener('click', findCoords);
+
+window.addEventListener('load', findWeather(40.7596198, -111.8867975), findWeatherCurrent(40.7596198, -111.8867975), loadLocalCities("salt_lake_city"));
